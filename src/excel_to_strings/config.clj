c0b1754cebd2e42fa781df087fe0416a46f075e3
hojunbaek <mr.hjbaek@ent-bc.com>
2023-03-15 11:52:15 +0900

    - 영문 미국 국기 표기 안되도록 수정

--- src/excel_to_strings/config.clj
+++ src/excel_to_strings/config.clj
@@ -9,7 +9,7 @@
 
 (defonce select-start-row 2) ; config 파일에서 불러오도록 수정
 (defonce output-android-files [{:code "en"
-                                :prefix "🇺🇸"
+                                :prefix ""
                                 :file "output/android/values/strings.xml"}
                                {:code "ko"
                                 :prefix ""
@@ -17,14 +17,14 @@
                                ])
 
 (defonce output-ios-files [{:code "en"
-                           :prefix "🇺🇸"
+                           :prefix ""
                            :file "output/ios/en.lproj/Localizable.strings"}
                           {:code "ko"
                            :prefix ""
                            :file "output/ios/ko.lproj/Localizable.strings"}])
 
 (defonce output-web-files [{:code "en"
-                            :prefix "🇺🇸"
+                            :prefix ""
                             :file "output/web/strings-en.json"}
                            {:code "ko"
                             :prefix ""
